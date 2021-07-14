d75acfd6f233047bf1735680f0dda293fc4d317c
Dev Chauhan <devc@ibm.com>
2021-07-14 13:24:38 -0600

    Add LeadTime module

--- src/components/LeadTimes.js
+++ src/components/LeadTimes.js
@@ -1,3 +1,43 @@
+import React, { useState } from "react";
+
 export default function LeadTimes() {
-    return <div></div>;
+  
+  const [Leadtime, setLeadTimes] = useState({data:"N/A"});
+  
+  const setDataVal = (dataVal) => {
+    localStorage.setItem("leadTime", JSON.stringify(dataVal));
   }
+  
+  const onSubmit = (e) => {
+   e.preventDefault();
+   let min = "minutes";
+   if(JSON.parse(localStorage.getItem("leadTime")) == "1") {
+      min = "minute";
+   } 
+   setLeadTimes({ ...Leadtime, data: JSON.parse(localStorage.getItem("leadTime")) + " " + min});
+  };
+
+  return (
+    <div className="container pt-5">
+      <h1>Lead Time</h1>
+      
+      <label htmlFor="author">From code pushed to code deployed: <b>{Leadtime.data} </b> </label>
+
+      <form onSubmit={onSubmit}>
+        <p>
+          <label htmlFor="title">Change Lead Time (in minutes)</label> 
+        </p>
+        <p>
+          <input
+            id="leadtime"
+            className="form-control"
+            type="text"
+            name="leadtime"
+            onChange={(e) => setDataVal(e.target.value)}
+          />
+        </p>
+        <button className="btn btn-primary">Update Lead Time</button>
+      </form>
+    </div>
+  );
+}
